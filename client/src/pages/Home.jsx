6fdefb28d05e839b00cbcc3a8fa669b5b1015516
adylitto <adylitto@gmail.com>
2023-04-24 11:26:50 +0200

    commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -24,11 +24,22 @@ const Home = () => {
     <><div class="flex flex-col mb-10">
       <h1 className='text-white flex-auto text-xl font-semibold text-center'>Welcome to DAprOd</h1>
       <p className='text-white text-center'>Be the producer of what you want to see</p>
+      <div className='mt-5'><center><iframe width="460" height="315" src="https://www.youtube.com/embed/cO3_rnP4UYg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></center></div> 
     </div>
+    <div class="flex flex-col m-10">
+      <h1 className='text-white flex-auto text-xl font-semibold text-center'>Team DAprOd</h1>
+      <p className='text-white text-center'>Our founders</p>
+    </div>
+    <div class="flex flex-col col-6 mb-10">
+
+    </div>
+
     <DisplayCampaigns
         title="All Campaigns"
         isLoading={isLoading}
-        campaigns={campaigns} /></>
+        campaigns={campaigns} />
+    
+  </>
   )
 }
 
